933a916a13baf5d1000a310feff3b987ed507f4b
v4he <vahe.vardanyan@laplateforme.io>
2026-05-03 21:54:03 +0200

    feat: implement word comparison logic in PHP

--- php/Game.php
+++ php/Game.php
@@ -1,14 +1,37 @@
-<?php 
+<?php
 header('Content-Type: application/json');
 
-
-
-if(isset($_POST['word'])){
-
-$word = $_POST['word'];
-    echo json_encode($word);
-}
+$db_word = 'cropar';
 
 
 
-?>
+
+if (isset($_POST['word'])) {
+
+
+    $word = $_POST['word'];
+
+
+    if ($word === $db_word) {
+        echo json_encode(true);
+    } else {
+        
+
+        for ($i = 0; $i < strlen($word); $i++) {
+                $letter = 'blue';
+            for ($y = 0; $y < strlen($db_word); $y++) {
+                if ($word[$i] === $db_word[$y] && $i === $y) {
+                    $letter = 'red';
+                }
+                else if($word[$y] === $db_word[$i]) {
+                    $letter = 'yellow';
+                }
+        
+                
+            };
+            $result[] = $letter;
+        }
+
+        echo json_encode($result);
+    }
+};
